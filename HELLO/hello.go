7315b26e4cde9326be004df46cf01332d3c30621
KentoImayoshi <sky133419@gmail.com>
2025-10-09 18:24:31 -0300

    Adicionando condicionais

--- HELLO/hello.go
+++ HELLO/hello.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	nome := "Douglas"
-	idade := 24
+	nome := "Kento"
 	versao := 1.1
-	fmt.Println("Olá sr.", nome, "sua idade é", idade)
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 
 	fmt.Println("1- Iniciar Monitoramento")
@@ -15,6 +14,15 @@ func main() {
 
 	var comando int
 	fmt.Scan(&comando)
-
 	fmt.Println("O comando escolhido foi", comando)
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs...")
+	} else if comando == 0 {
+		fmt.Println("Saindo do programa...")
+	} else {
+		fmt.Println("Não conheço este comando")
+	}
 }
